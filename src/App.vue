8f96ef6a149d06a49618ee249b533eb8e574119d
zehua <4528844@qq.com>
2021-05-10 18:14:35 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <HelloWorld msg="Hello Vue 3.0 + Element Plus + Vite" />
+</template>
+
+<script lang='ts'>
+import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
+  components: {
+    HelloWorld,
+  },
+  setup() {},
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.element-plus-logo {
+  width: 50%;
+}
+</style>
