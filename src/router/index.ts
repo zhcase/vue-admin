9692ed9a707a288efadc4f125aa19fae0b3a0a49
zehua <zehua@zehuadeiMac.local>
2021-05-20 17:47:25 +0800

    添加路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,21 @@
  * @Author: zeHua
  * @Date: 2021-05-19 17:55:48
  * @LastEditors: zeHua
- * @LastEditTime: 2021-05-19 17:55:50
+ * @LastEditTime: 2021-05-20 14:08:00
  * @FilePath: /admin-base/vue-admin/src/router/index.ts
  */
+import { RouteRecordRaw } from 'vue-router';
+
+
+import {createRouter,createWebHashHistory} from 'vue-router';
+import { basicRoutes, LoginRoute } from './routes';
+
+
+// app router
+
+const router=createRouter({
+    history:createWebHashHistory(import.meta.env.BASE_URL),
+    routes: (basicRoutes as unknown) as RouteRecordRaw[],
+    strict:true,
+    
+})
