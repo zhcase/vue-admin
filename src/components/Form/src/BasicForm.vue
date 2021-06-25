8f14b1ded31517b775c8dd4503e7e4b997745dd4
zehua <zehua@zehuadeiMac.local>
2021-06-25 18:12:25 +0800

    开发form 表单项目

--- src/components/Form/src/BasicForm.vue
+++ src/components/Form/src/BasicForm.vue
@@ -2,24 +2,53 @@
  * @Author: zeHua
  * @Date: 2021-06-23 16:01:39
  * @LastEditors: zeHua
- * @LastEditTime: 2021-06-25 10:47:00
+ * @LastEditTime: 2021-06-25 17:07:43
  * @FilePath: /vue-admin/src/components/Form/src/BasicForm.vue
 -->
 <template>
-  <div>2323 </div>
+  <ElForm>
+    <template v-for="schema in getSchema" :key="schema.field">
+      <FormItem :schema="schema" :getProps="getProps" />
+    </template>
+  </ElForm>
 </template>
 
-<script lang="ts">
-import { defineComponent, computed } from 'vue';
+<script lang="js">
+import { defineComponent, computed, unref,ref} from 'vue';
+import { ElForm,ElFormItem, } from 'element-plus'
+import FormItem from './components/FormItem.vue';
 import { basicProps } from './props';
 export default defineComponent({
+  components:{
+    ElForm,
+    ElFormItem,
+    FormItem
+  },
   props: basicProps,
+  
   setup(props) {
     console.log(props);
     const getProps = computed(() => {
-      return { ...props };
+        return { ...props, ...unref(propsRef) };
+      });
+     const propsRef=ref({});
+     
+
+    const getSchema = computed(() => {
+      const schemas = unref(getProps).schemas;
+      // for (const schema of schemas) {
+      //   const { defaultValue, component } = schema;
+      //   console.log(defaultValue);
+      //   console.log(component);
+      //  }
+      return schemas
     });
+    console.log(getSchema);
     console.log(getProps);
+    return {
+      getSchema,
+      getProps
+    }
   },
 });
 </script>
---
+++ src/components/Form/src/components/FormItem.vue
@@ -0,0 +1,57 @@
+<!--
+ * @Author: zeHua
+ * @Date: 2021-06-25 15:15:49
+ * @LastEditors: zeHua
+ * @LastEditTime: 2021-06-25 17:22:38
+ * @FilePath: /vue-admin/src/components/Form/src/components/FormItem.vue
+-->
+<template>
+  <div class="">122 </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, toRefs, computed } from 'vue';
+import { useItemLabelWidth } from '../hooks/useLabelWidth';
+export default defineComponent({
+  props: {
+    schema: {
+      type: Object,
+      default: () => ({}),
+    },
+    formProps: {
+      type: Object,
+      default: () => ({}),
+    },
+    formModel: {
+      type: Object,
+      default: () => ({}),
+    },
+    allDefaultValues: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  setup(props) {
+    const { schema, formProps } = toRefs(props);
+    const itemLabelWidthProp = useItemLabelWidth(schema, formProps);
+    console.log(itemLabelWidthProp);
+
+    console.log(schema);
+    const getValues = computed(() => {
+      const { allDefaultValues, formModel, schema } = props;
+      const { mergeDynamicData } = props.formProps;
+      return {
+        field: schema.field,
+        model: formModel,
+        values: {
+          ...mergeDynamicData,
+          ...allDefaultValues,
+          ...formModel,
+        },
+        schema: schema,
+      };
+    });
+    console.log(getValues);
+  },
+});
+</script>
